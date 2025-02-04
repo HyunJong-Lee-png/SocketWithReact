35db6dc51bcb2213c43ccf975ba594acee32d757
HyunJongLee <dlguswhd1009@naver.com>
2025-02-04 13:25:15 +0900

    update cors policy

--- Socket-Server/server.ts
+++ Socket-Server/server.ts
@@ -3,14 +3,26 @@ import express from 'express';
 import { Server } from 'socket.io';
 import mongoose from "mongoose";
 import socketIO from './socket.io';
+import cors from 'cors';
 
 const PORT = process.env.PORT || 8000;
 
 const app = express();
+app.use(cors({
+  origin: [
+    "https://socket-io-kakao-talk.vercel.app", // 클라이언트 도메인
+    "http://localhost:5173",                  // 개발용 로컬 도메인
+  ],
+  methods: ["GET", "POST"],                   // 허용할 HTTP 메서드
+  credentials: true,                          // 쿠키/인증 정보 허용
+}));
 const httpServer = createServer(app);
 const socketServer = new Server(httpServer, {
   cors: {
-    origin: 'https://socket-io-kakao-talk.vercel.app'
+    origin: [
+      "https://socket-io-kakao-talk.vercel.app", // 클라이언트 도메인
+      "http://localhost:5173",                  // 개발용 로컬 도메인
+    ],
   }
 });
 
